a0a4a09d6f27aec21e2fe1909c2f2c49d3036d9c
supmit <gour2502@gmail.com>
2015-03-18 11:12:17 +0530

    Restructuring project.

---
+++ sarvo-aem-services/src/main/java/com/sarvo/aemproject/services/osgi/Activator.java
@@ -0,0 +1,19 @@
+package com.sarvo.aemproject.services.osgi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.squeakysand.osgi.framework.BasicBundleActivator;
+
+/**
+ * Bundle activator for com.sarvo.aem - sarvo-aem-services.
+ */
+public class Activator extends BasicBundleActivator {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
+
+    public Activator() {
+		super(LOG);
+	}
+
+}
